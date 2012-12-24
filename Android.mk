d900a5153bda0da303b423f2075f00f4d6f4fbe6
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-12-24 17:04:34 -0600

    display-legacy: makefile: fix build for caf variant & disable tilerenderer
    Change-Id: Ib03bffe226f1cf94c792fa4b5c1d8f9943d2838c

--- Android.mk
+++ Android.mk
@@ -2,15 +2,15 @@
 #libs to be built for QCOM targets only
 
 ifeq ($(BOARD_USES_LEGACY_QCOM),true)
+
 display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils
+#display-hals += libtilerenderer
 
 ifneq ($(TARGET_PROVIDES_LIBLIGHTS),true)
 display-hals += liblight
 endif
 
-endif
-
-display-hals += libtilerenderer
-
 include $(call all-named-subdir-makefiles,$(display-hals))
+
+endif
