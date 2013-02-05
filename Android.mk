4ed3f8b0871f2dfa6c80732c0fb51bf2107f5651
Konsta <konsta09@gmail.com>
2013-02-05 23:19:37 +0200

    allow to build tilerenderer
    Change-Id: I1eb6d19d17ff3cff1f621d8ace6fde7c64850799
    
    Conflicts:
    	Android.mk

--- Android.mk
+++ Android.mk
@@ -4,7 +4,7 @@
 ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
 
 display-hals := libgralloc libgenlock libcopybit
-display-hals += libhwcomposer liboverlay libqdutils
+display-hals += libhwcomposer liboverlay libqdutils libtilerenderer
 
 ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
 display-hals += liblight
