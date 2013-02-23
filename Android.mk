135b4113cce9b09949257fa4bc69d33f5171f6eb
Konsta <konsta09@gmail.com>
2013-02-23 16:14:15 +0200

    tilerenderer: fix include path
    Change-Id: I8707b92de2e0ca02f4e6475919ce8449f2985666

--- Android.mk
+++ Android.mk
@@ -4,8 +4,7 @@
 ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
 
 display-hals := libgralloc libgenlock libcopybit
-display-hals += libhwcomposer liboverlay libqdutils
-display-hals += libtilerenderer
+display-hals += libhwcomposer liboverlay libqdutils libtilerenderer
 
 ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
 display-hals += liblight
